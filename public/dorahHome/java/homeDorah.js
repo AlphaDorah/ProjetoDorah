de52886475e3125ca9e375ce84873c6554f1e693
William Silva <william.deoliveirasilva@outlook.com>
2023-10-09 16:46:41 -0300

    Inclusão da função para receber o tema escrito pelo usuário

--- public/dorahHome/java/homeDorah.js
+++ public/dorahHome/java/homeDorah.js
@@ -1,20 +1,23 @@
 let i = 0;
-let count = 0;
 let placeholder = "";
 const txt = "Digite um tema...";
-const theme1 = "Independência do Brasil";
-const theme2 = "Segunda Guerra Mundial";
-const theme3 = "Produtos notáveis"
-const theme4 = "Império Romano"
 const speed = 100;
-function escrita() {
+let inputTheme;
+
+function writteanimation()
+{
         placeholder += txt.charAt(i);
-        document.getElementById("theme").setAttribute("placeholder", placeholder);
+        document.getElementById("themeInput").setAttribute("placeholder", placeholder);
         i++;
-        setTimeout(escrita, speed);
-    }
+        setTimeout(writteanimation, speed);
+}
 
+    writteanimation();
 
+function getInput()
+{
+    var input = document.getElementById("themeInput");
 
-
-escrita();
+    inputTheme = input.value;
+    alert("Tema inserido: " +inputTheme);
+}
