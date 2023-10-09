72648ff7e8b3ac9fb5b96c845aaa441a53b5ff91
faduzin <eric.fadul@gmail.com>
2023-10-08 23:25:41 -0300

    Changes by willi

--- public/dorahHome/java/homeDorah.js
+++ public/dorahHome/java/homeDorah.js
@@ -1,7 +1,7 @@
 let i = 0;
 let placeholder = "";
-const txt = "Digite um tema";
-const speed = 120;
+const txt = "Digite um tema...";
+const speed = 100;
 
 function escrita(){
     placeholder += txt.charAt(i);
