98a923fb8f0198ffe6f0f0ac0d67e1549e2c6efb
faduzin <eric.fadul@gmail.com>
2023-10-08 22:58:17 -0300

    Homedorah s

---
+++ public/dorahHome/java/homeDorah.js
@@ -0,0 +1,13 @@
+let i = 0;
+let placeholder = "";
+const txt = "Digite um tema";
+const speed = 120;
+
+function escrita(){
+    placeholder += txt.charAt(i);
+    document.getElementById("email").setAttribute("placeholder", placeholder);
+    i++;
+    setTimeout(escrita, speed);
+}
+
+escrita();
