8681d640c0aad406849fe46eed1d7700d53efa48
TheSmileyDroid <ghsilha@gmail.com>
2023-10-22 11:57:44 -0300

    format test

--- tests/dorahSearch/test_google_api.py
+++ tests/dorahSearch/test_google_api.py
@@ -2,11 +2,13 @@ import src.dorahSearch.google_api as google
 
 
 def test_get_links_quantity():
-  assert len(google.get_links('Produtos Notáveis',
-                              google._google_search_test)) > 0
+    assert (
+        len(google.get_links("Produtos Notáveis", google._google_search_test))
+        > 0
+    )
 
 
 def test_get_links_correctly():
-  links = google.get_links('Produtos Notáveis', google._google_search_test)
+    links = google.get_links("Produtos Notáveis", google._google_search_test)
 
-  assert 'youtube' not in links and 'wiki' not in links
+    assert "youtube" not in links and "wiki" not in links
